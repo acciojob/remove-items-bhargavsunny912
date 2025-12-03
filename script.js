1a15812b289892fad3f61f3a682faf0d0df0f936
Bhargav Penta <150350442+bhargavsunny912@users.noreply.github.com>
2025-12-03 12:28:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let select=document.getElementById("colorSelect");
+let button=document.querySelector("input");
+
+button.addEventListener("click",()=>{
+	select.remove(select.selectedIndex);
+})
